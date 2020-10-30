f84a57a2008f61fa732fd77b859c1ae8f6a1e55c
FinnMulder <70947163+FinnMulder@users.noreply.github.com>
2020-10-30 20:12:24 +0100

    hoi

---
+++ pythonachievements ifstatements.py
@@ -0,0 +1,64 @@
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: /Users/finnmulder/Documents/pythonachievements/ifstatements.py ===
+Welkom, wat is jou naam?
+finn
+Welkom, finn Laten we beginnen.
+(1) Als je alarm gaat, ga je dan gelijk je bed uit of niet? ja/nee
+ja
+Top, vroege vogel dus.
+(2) Ga je eerst douchen of eten?
+eten
+Lekker toch.
+(3) Heb je je tas al in de avond ingepakt of niet? ja/nee
+nee
+Kost wel tijd in de ochtend.
+Wil je het programma herhalen? ja/nee
+ja
+Welkom, wat is jou naam?
+finn
+Welkom, finn Laten we beginnen.
+(1) Als je alarm gaat, ga je dan gelijk je bed uit of niet? ja/nee
+nee
+Daar herken ik mezelf in!
+(2) Ga je eerst douchen of eten?
+douchen
+Top! Dat doe ik ook.
+(3) Heb je je tas al in de avond ingepakt of niet? ja/nee
+ja
+Top! Ik ook.
+Wil je het programma herhalen? ja/nee
+nee
+Het programma word afgesloten.
+>>> 
+==== RESTART: /Users/finnmulder/Documents/pythonachievements/ifstatements.py ===
+Welkom, hoe heet jij?
+finn
+Welkom, finn Laten we beginnen.
+(1) Als je wekker gaat, ga je dan gelijk je bed uit of niet? ja/nee
+ja
+Top, vroege vogel dus.
+(2) Ga je eerst douchen of eten?
+eten
+Lekker toch.
+(3) Heb je je tas al in de avond ingepakt of niet? ja/nee
+
+==== RESTART: /Users/finnmulder/Documents/pythonachievements/ifstatements.py ===
+Welkom, hoe heet jij?
+finn
+Welkom, finn Laten we beginnen.
+(1) Als je wekker gaat, ga je dan gelijk je bed uit of niet? ja/nee
+ja
+Top, vroege vogel dus.
+(2) Ga je eerst douchen of eten?
+eten
+Lekker toch.
+(3) Heb je al bedacht welke kleren je aan doet? ja/nee
+nee
+Kost wel tijd in de ochtend.
+Wil je het programma herhalen? ja/nee
+nee
+Het programma word afgesloten.
+>>> 
