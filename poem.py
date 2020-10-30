5bfd4b73e965f669f95bed75f5451bbd1fc2ffa2
Finn Mulder <finnmulder@MacBook-Pro-van-Finn.local>
2020-10-30 14:47:36 +0100

    doei

---
+++ poem.py
@@ -0,0 +1,7 @@
+vers1 = "hallo ik ben Finn en ik ben bang voor een spin"
+vers2 = "ik woon in een huis en ik zit nooit voor de buis"
+vers3 = "ik zit op voetbal maar pas op voordat ik je moeder bal"
+
+
+
+Print("vers1, vers2, vers3, vers4")
