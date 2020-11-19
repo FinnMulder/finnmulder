ad92394f5e2fbd071ab9ded6657f6f53b54756f6
FinnMulder <70947163+FinnMulder@users.noreply.github.com>
2020-11-19 11:35:10 +0100

    Update poem.py

--- poem.py
+++ poem.py
@@ -1,6 +1,6 @@
 vers1 = "hallo ik ben Finn en ik ben bang voor een spin"
 vers2 = "ik woon in een huis en ik zit nooit voor de buis"
-vers3 = "ik zit op voetbal maar pas op voordat ik je moeder bal"
+vers3 = "ik zit op voetbal maar pas op voor die knal bal"
 
 
 
